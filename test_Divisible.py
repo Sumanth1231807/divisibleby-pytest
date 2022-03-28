64a0d4e5dba55b5a7cdc90c58f1e1475f2d19df6
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-28 12:26:57 +0530

    divisibleby pytest successfully

--- test_Divisible.py
+++ test_Divisible.py
@@ -3,24 +3,24 @@ import Divisible
 
 
 def test_divisible_5():
-    x = 20
+    x = 18
     res = Divisible.divisible_5(x)
     assert res == True
 
 
 def test_divisible_7():
-    x = 21
+    x = 18
     res = Divisible.divisible_7(x)
     assert res == True
 
 
 def test_divisible_9():
-    x = 18
+    x = 17
     res = Divisible.divisible_9(x)
     assert res == True
 
 
 def test_divisible_10():
-    x = 20
+    x = 15
     res = Divisible.divisible_10(x)
     assert res == True
